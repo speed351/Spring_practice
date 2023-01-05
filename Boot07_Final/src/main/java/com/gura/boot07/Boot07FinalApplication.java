c625811aa9f7f1d5efcab5ae0be1610f632ea63b
park <zpzp351@naver.com>
2023-01-05 11:17:57 +0900

    Boot07 필기 추가

--- Boot07_Final/src/main/java/com/gura/boot07/Boot07FinalApplication.java
+++ Boot07_Final/src/main/java/com/gura/boot07/Boot07FinalApplication.java
@@ -9,8 +9,20 @@ import org.springframework.context.annotation.PropertySource;
 /*
  * 
  * 이 프로젝트로 xxx.war 파일을 만들어서 외부 tomcat으로 실행하려면 
- * SpringBootServletInitializer을 상속받아서 설정해야 한다.
- *
+ * 	 1. SpringBootServletInitializer을 상속받아서 설정해야 한다.
+ *   2. pom.xml 에 <packaging>war</packaging> 추가
+ *   3. pom.xml dependency 추가
+ *      <dependency>
+          <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-tomcat</artifactId>
+            <scope>provided</scope>
+      	</dependency>
+    4.  
+       [프로젝트 우클릭] → [Run As]  → [Maven build...]를 차례로 선택한다.
+
+        - Goals에 package를 입력하고, Profiles에 pom.xml을 지워주고, Run 버튼을 클릭한다.
+        - console에 BUILD SUCCESS가 표시되고, 프로젝트 폴더 안 target 폴더에는 war 파일이 생성되어 있다.
+
  */
 @SpringBootApplication
 @PropertySource("classpath:custom.properties")
